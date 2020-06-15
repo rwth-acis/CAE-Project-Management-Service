1aba775f7a4a91d58dab7c01260c301554564b6c
Philipp Dolif <git.phil.cd@gmail.com>
2020-06-15 16:40:04 +0200

    Interface for CAE Model Persistence Service

--- src/main/i5/las2peer/services/projectManagementService/ProjectManagementService.java
+++ src/main/i5/las2peer/services/projectManagementService/ProjectManagementService.java
@@ -2,10 +2,20 @@ package i5.las2peer.services.projectManagementService;
 
 import i5.las2peer.restMapper.RESTService;
 import i5.las2peer.restMapper.annotations.ServicePath;
+import i5.las2peer.services.projectManagementService.auth.AuthManager;
 import i5.las2peer.services.projectManagementService.database.DatabaseManager;
 import i5.las2peer.services.projectManagementService.github.GitHubHelper;
+import i5.las2peer.services.projectManagementService.project.Project;
+import i5.las2peer.services.projectManagementService.project.User;
 import i5.las2peer.services.projectManagementService.reqbaz.ReqBazHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import i5.las2peer.api.ManualDeployment;
+import i5.las2peer.logging.L2pLogger;
 
 /**
  * Project Management Service
@@ -66,5 +76,73 @@ public class ProjectManagementService extends RESTService {
 	
 	public DatabaseManager getDbm(){
 		return dbm;
+	}
+	
+	/**
+	 * Method used by CAE Model Persistence Service.
+	 * When calling this method, the agent needs to be a real user.
+	 * @param versionedModelId Id of the versioned model, where the permissions should be checked for.
+	 * @return Whether the user calling the method has the permission to commit to the versioned model.
+	 */
+	public boolean hasCommitPermission(int versionedModelId) {
+		Connection connection = null;
+        try {
+        	AuthManager authManager = new AuthManager((L2pLogger) this.getLogger(), this.getDbm());
+			User user = authManager.getUser();
+			
+			connection = dbm.getConnection();
+			
+			// get project where the versioned model belongs to
+			// the versioned model should at least be connected with a component
+			// but maybe if the project got deleted and the component still exists then no
+			// project can be found, thus we need to check if we get a query result
+			PreparedStatement statement = connection
+					.prepareStatement("SELECT Project.* FROM Component, ProjectToComponent, Project " +
+			                          "WHERE Component.versionedModelId = ? AND " +
+							          "ProjectToComponent.componentId = Component.id AND ProjectToComponent.projectId = Project.id;");
+			statement.setInt(1, versionedModelId);
+			
+			ResultSet queryResult = statement.executeQuery();
+			if(!queryResult.next()) {
+				// could not find a project which is connected to the versioned model
+				return false;
+			}
+			
+			int projectId = queryResult.getInt("id");
+			statement.close();
+			
+			// get Project object
+			Project project = new Project(projectId, connection);
+			
+			// check if user is a member of the project
+			if(project.hasUser(user.getId(), connection)) {
+				// user is a member of the project where the versioned model belongs to
+				return true;
+			} else {
+				// user is no member of the project where the versioned model belongs to
+				return false;
+			}
+        } catch (Exception e) {
+        	return false;
+        } finally {
+        	try {
+				if(connection != null) connection.close();
+			} catch (SQLException e) {
+				return false;
+			}
+        }
+	}
+	
+	/**
+	 * Method used by CAE Model Persistence Service
+	 * @return Whether the user calling the method is anonymous or not.
+	 */
+	public boolean isAnonymous() {
+		try {
+			AuthManager authManager = new AuthManager((L2pLogger) this.getLogger(), this.getDbm());
+			return authManager.isAnonymous();
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
