7be2392d8326f9fd9fc26b5742652b68124517b3
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-03-07 22:51:11 +0100

    Merge branch 'ba-erdzan' of https://github.com/rwth-acis/CAE-Project-Management-Service into ba-erdzan

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
-# note: not sh or zsh compatible
+#!/usr/bin/env bash
+
 set -e
-#set -o verbose # echo all commands before execution
 
 # print all comands to console if DEBUG is set
 if [[ ! -z "${DEBUG}" ]]; then
     set -x
 fi
+NODE_ID_SEED=${NODE_ID_SEED:-$RANDOM}
+EXTRA_ETH_WAIT=${EXTRA_ETH_WAIT:-30}
+CONFIG_ENDPOINT_WAIT=${CONFIG_ENDPOINT_WAIT:-21600}
+ETH_PROPS_DIR=/src/etc/
+ETH_PROPS=i5.las2peer.registry.data.RegistryConfiguration.properties
+function waitForEndpoint {
+    /src/wait-for-command/wait-for-command.sh -c "nc -z ${1} ${2:-80}" --time ${3:-10} --quiet
+}
 
+function host { echo ${1%%:*}; }
+function port { echo ${1#*:}; }
 # set some helpful variables
 export SERVICE_PROPERTY_FILE='etc/i5.las2peer.services.projectManagementService.ProjectManagementService.properties'
 export WEB_CONNECTOR_PROPERTY_FILE='etc/i5.las2peer.connectors.webConnector.WebConnector.properties'
@@ -17,61 +26,6 @@ export SERVICE_CLASS=$(awk -F "=" '/service.class/ {print $2}' gradle.properties
 export SERVICE=${SERVICE_NAME}.${SERVICE_CLASS}@${SERVICE_VERSION}
 export CREATE_DB_SQL='database/ProjectManagementService_Database.sql'
 export MYSQL_DATABASE='commedit'
-
-# when migrating (deploying smart contracts, done by boot node),
-# after eth client is seemingly ready, wait for this many extra seconds
-# (because e.g. account unlocking takes time)
-EXTRA_ETH_WAIT=${EXTRA_ETH_WAIT:-30}
-
-# max wait for bootstrapping node to perform migration and share its config
-# with trivial block time, this takes just a few seconds; with higher block
-# times, this can be e.g. 15 minutes (but possibly much more, depending on
-# difficulty)
-# ... really, at this point we might as well wait forever (until the user
-# kills us), but let's go for a solid six hours
-CONFIG_ENDPOINT_WAIT=${CONFIG_ENDPOINT_WAIT:-21600}
-
-NODE_ID_SEED=${NODE_ID_SEED:-$RANDOM}
-
-ETH_PROPS_DIR=/src/etc/
-ETH_PROPS=i5.las2peer.registry.data.RegistryConfiguration.properties
-
-LA_ETH_PROPS_FILE='/app/etc/i5.las2peer.services.learningAnalyticsVerification.LaRegistryConfiguration.properties'
-
-function waitForEndpoint {
-    /src/wait-for-command/wait-for-command.sh -c "nc -z ${1} ${2:-80}" --time ${3:-10} --quiet
-}
-
-function host { echo ${1%%:*}; }
-function port { echo ${1#*:}; }
- 
- function truffleMigrateLa { 
-    echo Starting truffle migration of LA contracts...
-    cd /src/la-registry-contracts
-    ./node_modules/.bin/truffle migrate --network docker_boot 2>&1 | tee migration-la.log
-    echo done. Setting contract addresses in config file ...
-    # yeah, this isn't fun:
-    cat migration-la.log | grep -A5 "\(Deploying\|Replacing\|contract address\) \'\(ConsentRegistry\|VerificationRegistry\)\'" | grep '\(Deploying\|Replacing\|contract address\)' | tr -d " '>:" | sed -e '$!N;s/\n//;s/Deploying//;s/Replacing//;s/contractaddress/Address = /;s/./\l&/' >> "${LA_ETH_PROPS_FILE}"
-    cp migration-la.log /src/las2peer/node-storage/migration-la.log
-    echo done. 
- }
- 
- # Delete this for cluster deployment!
-if [ -n "$LAS2PEER_CONFIG_ENDPOINT" ]; then
-    echo Attempting to autoconfigure registry blockchain parameters ...
-    if waitForEndpoint $(host ${LAS2PEER_CONFIG_ENDPOINT}) $(port ${LAS2PEER_CONFIG_ENDPOINT}) $CONFIG_ENDPOINT_WAIT; then
-        echo "Port is available (but that may just be the Docker daemon)."
-        echo Downloading ...
-        wget --quiet --tries=inf "http://${LAS2PEER_CONFIG_ENDPOINT}/${ETH_PROPS}" -O "${ETH_PROPS_DIR}${ETH_PROPS}"
-        echo done.
-    else
-        echo Registry configuration endpoint specified but not accessible. Aborting.
-        exit 1
-    fi
-fi
-
-
-NODE_ID_SEED=${NODE_ID_SEED:-$RANDOM}
 
 # check mandatory variables
 [[ -z "${MYSQL_USER}" ]] && \
@@ -106,6 +60,7 @@ NODE_ID_SEED=${NODE_ID_SEED:-$RANDOM}
 [[ -z "${OIDC_PROVIDERS}" ]] && export OIDC_PROVIDERS='https://api.learning-layers.eu/o/oauth2,https://accounts.google.com'
 
 # configure service properties
+
 function set_in_service_config {
     sed -i "s?${1}[[:blank:]]*=.*?${1}=${2}?g" ${SERVICE_PROPERTY_FILE}
 }
@@ -136,8 +91,6 @@ set_in_web_config crossOriginResourceMaxAge ${CROSS_ORIGIN_RESOURCE_MAX_AGE}
 set_in_web_config enableCrossOriginResourceSharing ${ENABLE_CROSS_ORIGIN_RESOURCE_SHARING}
 set_in_web_config oidcProviders ${OIDC_PROVIDERS}
 
-
-
 # ensure the database is ready
 while ! mysqladmin ping -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} --silent; do
     echo "Waiting for mysql at ${MYSQL_HOST}:${MYSQL_PORT}..."
@@ -151,25 +104,18 @@ if ! mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -
     mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < ${CREATE_DB_SQL}
 fi
 
-# wait for any bootstrap host to be available
-if [[ ! -z "${BOOTSTRAP}" ]]; then
-    echo "Waiting for any bootstrap host to become available..."
-    for host_port in ${BOOTSTRAP//,/ }; do
-        arr_host_port=(${host_port//:/ })
-        host=${arr_host_port[0]}
-        port=${arr_host_port[1]}
-        if { </dev/tcp/${host}/${port}; } 2>/dev/null; then
-            echo "${host_port} is available. Continuing..."
-            break
-        fi
-    done
+if [ -n "$LAS2PEER_CONFIG_ENDPOINT" ]; then
+    echo Attempting to autoconfigure registry blockchain parameters ...
+    if waitForEndpoint $(host ${LAS2PEER_CONFIG_ENDPOINT}) $(port ${LAS2PEER_CONFIG_ENDPOINT}) $CONFIG_ENDPOINT_WAIT; then
+        echo "Port is available (but that may just be the Docker daemon)."
+        echo Downloading ...
+        wget --quiet --tries=inf "http://${LAS2PEER_CONFIG_ENDPOINT}/${ETH_PROPS}" -O "${ETH_PROPS_DIR}${ETH_PROPS}"
+        echo done.
+    else
+        echo Registry configuration endpoint specified but not accessible. Aborting.
+        exit 1
+    fi
 fi
-
-# prevent glob expansion in lib/*
-set -f
-LAUNCH_COMMAND='java -cp lib/* i5.las2peer.tools.L2pNodeLauncher -s service -p '"${LAS2PEER_PORT} ${SERVICE_EXTRA_ARGS}"
-if [[ ! -z "${BOOTSTRAP}" ]]; then
-    LAUNCH_COMMAND="${LAUNCH_COMMAND} -b ${BOOTSTRAP}"
 
 if [ -n "$LAS2PEER_BOOTSTRAP" ]; then
     if waitForEndpoint $(host ${LAS2PEER_BOOTSTRAP}) $(port ${LAS2PEER_BOOTSTRAP}) 600; then
@@ -194,39 +140,36 @@ function selectMnemonic {
     fi
 }
 
-
 #prepare pastry properties
 echo external_address = $(curl -s https://ipinfo.io/ip):${LAS2PEER_PORT} > etc/pastry.properties
 
-# start the service within a las2peer node
-if [[ -z "${@}" ]]
-then
-  exec ${LAUNCH_COMMAND} startService\("'""${SERVICE}""'", "'""${SERVICE_PASSPHRASE}""'"\) startWebConnector
-    # if [ -n "$LAS2PEER_ETH_HOST" ]; then
-    # echo ... using ethereum boot procedure: 
-    # java $(echo $ADDITIONAL_JAVA_ARGS) \
-    #     -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher \
-    #     --service-directory service \
-    #     --port $LAS2PEER_PORT \
-    #     $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") \
-    #     --node-id-seed $NODE_ID_SEED \
-    #     --ethereum-mnemonic "$(selectMnemonic)" \
-    #     $(echo $ADDITIONAL_LAUNCHER_ARGS) \
-    #     uploadStartupDirectory \
-	# 	startService\("'""${SERVICE}""'", "'""${SERVICE_PASSPHRASE}""'"\) \
-	# 	startWebConnector \
-    #     "node=getNodeAsEthereumNode()" "registry=node.getRegistryClient()" "n=getNodeAsEthereumNode()" "r=n.getRegistryClient()"
-    # else
-    #  echo ... using non-ethereum boot procedure:
-    # java $(echo $ADDITIONAL_JAVA_ARGS) \
-    #     -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher \
-    #     --service-directory service \
-    #     --port $LAS2PEER_PORT \
-    #     $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") \
-    #     --node-id-seed $NODE_ID_SEED \
-    #     $(echo $ADDITIONAL_LAUNCHER_ARGS) \
-    #     startWebConnector
-    # fi
+echo Starting las2peer node ...
+if [ -n "$LAS2PEER_ETH_HOST" ]; then
+    echo ... using ethereum boot procedure: 
+    java $(echo $ADDITIONAL_JAVA_ARGS) \
+        -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher \
+        --service-directory service \
+        --port $LAS2PEER_PORT \
+        $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") \
+        --node-id-seed $NODE_ID_SEED \
+        --ethereum-mnemonic "$(selectMnemonic)" \
+        $(echo $ADDITIONAL_LAUNCHER_ARGS) \
+        startService\("'""${SERVICE}""'", "'""${SERVICE_PASSPHRASE}""'"\) \
+        startWebConnector \
+        "node=getNodeAsEthereumNode()" "registry=node.getRegistryClient()" "n=getNodeAsEthereumNode()" "r=n.getRegistryClient()" \
+        $(echo $ADDITIONAL_PROMPT_CMDS) \
+        interactive
 else
-  exec ${LAUNCH_COMMAND} ${@}
+    echo ... using non-ethereum boot procedure:
+    java $(echo $ADDITIONAL_JAVA_ARGS) \
+        -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher \
+        --service-directory service \
+        --port $LAS2PEER_PORT \
+        $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") \
+        --node-id-seed $NODE_ID_SEED \
+        $(echo $ADDITIONAL_LAUNCHER_ARGS) \
+        startService\("'""${SERVICE}""'", "'""${SERVICE_PASSPHRASE}""'"\) \
+        startWebConnector \
+        $(echo $ADDITIONAL_PROMPT_CMDS) \
+        interactive
 fi
